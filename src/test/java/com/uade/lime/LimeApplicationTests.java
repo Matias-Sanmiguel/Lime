3fae9260cf721b04cce230eb48b8081971eb7231
Matias Adrian Sanmiguel <masanmiguel@uade.edu.ar>
2026-08-12 21:19:06 -0300

    Initial commit

---
+++ src/test/java/com/uade/lime/LimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.lime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
