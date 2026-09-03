1bff9018f4813df033f7a42c89de35f43fe2bf5d
Micaela Cembal <micaelacembal@gmail.com>
2026-09-02 21:10:53 -0300

    addImage devuelve 404 con notFound() cuando la propiedad no existe

--- src/main/java/com/uade/lime/property/controller/PropertyController.java
+++ src/main/java/com/uade/lime/property/controller/PropertyController.java
@@ -9,8 +9,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -70,8 +70,9 @@ public class PropertyController {
 
     @PostMapping("/{id}/images")
     public ResponseEntity<ImageResponse> addImage(@PathVariable Long id, @Valid @RequestBody CreateImageRequest request) {
-        ImageResponse created = service.addImage(id, request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+    return service.addImage(id, request)
+            .map(image -> ResponseEntity.status(HttpStatus.CREATED).body(image))
+            .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @GetMapping("/{id}")
