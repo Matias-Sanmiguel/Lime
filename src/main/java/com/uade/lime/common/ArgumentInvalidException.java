30ab5510b166f7e1d8c8db7cf2625a8ca7bfd06f
Damian Bussetto <damianbusse04@gmail.com>
2026-09-08 19:07:35 -0300

    Implementar ArgumentInvalidException y usarla en AuthService
    - Completar ArgumentInvalidException (estaba vacía 0 bytes)
    - Agregar handler 400 ProblemDetail en ApiExceptionHandler
    - Reemplazar ResponseStatusException(BAD_REQUEST) en AuthService
    
    Closes #22

---
+++ src/main/java/com/uade/lime/common/ArgumentInvalidException.java
@@ -0,0 +1,8 @@
+package com.uade.lime.common;
+
+public class ArgumentInvalidException extends RuntimeException {
+
+    public ArgumentInvalidException(String message) {
+        super(message);
+    }
+}
