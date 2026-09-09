5077d263561485dd8496e8f5f52c56e809422564
Micaela Cembal <micaelacembal@gmail.com>
2026-09-08 22:28:11 -0300

    Agrego @Repository antes de implementar las interfaces como pide la consigna

--- src/main/java/com/uade/lime/property/repository/InquiryRepository.java
+++ src/main/java/com/uade/lime/property/repository/InquiryRepository.java
@@ -8,9 +8,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.uade.lime.property.model.Inquiry;
 
+@Repository
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 
     @Query("SELECT i FROM Inquiry i JOIN FETCH i.property p WHERE p.ownerId = :ownerId ORDER BY i.createdAt DESC")
